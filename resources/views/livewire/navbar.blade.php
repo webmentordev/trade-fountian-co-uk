f37b44f2d763bc619bc73b5c9e7ab1a1755b65de
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2023-08-17 15:41:08 +0500

    admin checks, auth nav behaviour

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -11,11 +11,31 @@
             <a class="mx-4 py-1 hover:underline transition-all" href="#">About</a>
         </ul>
         
-        <div x-data="{ open: false }">
-            <div x-on:click="open = !open" class="p-2 bg-gray-100 rounded-full flex items-center cursor-pointer">
-                <img src="https://api.iconify.design/mingcute:shopping-bag-2-fill.svg?color=%232f322f" alt="Cart Icon" width="25">
-                {{-- <span class="bg-black text-white py-1 px-2 rounded-full text-sm">{{ 2 }}</span> --}}
+        <div class="flex items-center">
+            <ul class="font-semibold">
+                @auth
+                    @if (auth()->user()->is_admin)
+                        <a class="pr-5 py-1 hover:underline transition-all border-r border-gray-100" href="{{ route('dashboard') }}">Dashboard</a>
+                    @endif
+                    <a class="mx-4 pr-4 py-1 hover:underline transition-all border-r border-gray-100" href="#">Client</a>
+                @endauth
+                @guest
+                    <a class="pr-5 py-1 hover:underline transition-all border-r border-gray-100" href="{{ route('login') }}">Login</a>
+                    <a class="mx-4 py-1 hover:underline transition-all" href="{{ route('register') }}">Signup</a>
+                @endguest
+            </ul>
+            <div x-data="{ open: false }" class="ml-3">
+                <div x-on:click="open = !open" class="p-2 bg-gray-100 rounded-full flex items-center cursor-pointer">
+                    <img src="https://api.iconify.design/mingcute:shopping-bag-2-fill.svg?color=%232f322f" alt="Cart Icon" width="25">
+                    {{-- <span class="bg-black text-white py-1 px-2 rounded-full text-sm">{{ 2 }}</span> --}}
+                </div>
             </div>
+            @auth
+                <form action="{{ route('logout') }}" method="post">
+                    @csrf
+                    <button type="submit" class="bg-black py-2 ml-3 px-5 text-white font-semibold">Logout</button>
+                </form>
+            @endauth
         </div>
     </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ Route::get('/', Home::class)->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'is_admin'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified', 'is_admin'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
