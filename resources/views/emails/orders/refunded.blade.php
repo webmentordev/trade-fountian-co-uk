097c32c828fe8f4a611d64be07f9b7cd1f575cde
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2023-12-21 13:54:40 +0500

    fixed refunded email text

--- resources/views/emails/orders/refunded.blade.php
+++ resources/views/emails/orders/refunded.blade.php
@@ -3,13 +3,13 @@
 
 Dear {{ $name }},
 
-We hope this message finds you well. We would like to inform you that the refund for your order has been successfully processed.
+We hope this message finds you well. We would like to inform you that the full amount refund for your order has been successfully processed.
 
 Here are the key details of the order which was refunded:
 
 **Order ID**:  {{ $id }}
 
-The refunded amount will be credited back to your original payment method. Please allow a few business days for the transaction to reflect in your account.
+The refunded amount should be credited back to your original payment method. Please allow a few business days for the transaction to reflect in your account.
 
 If you have any questions or if there's anything else we can assist you with, please don't hesitate to reach out to our customer support team at **support@tradefountain.co.uk**.
 
